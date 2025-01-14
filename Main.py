eae961f0d838bdfe7dc05287ff1b1218d3802218
M2SiCKneSs <145918930+M2SiCKneSs@users.noreply.github.com>
2025-01-14 16:15:04 +0200

    Delete Main.py
    no need

--- Main.py
+++
@@ -1,9 +0,0 @@
-import client
-from server import tcpserver
-
-#define server ip and server port
-serverIP = '192.168.1.24'  # Replace with your machine's IP
-serverPort = 12000
-
-tcpserver(serverIP,serverPort)
-client.send_data_tcp(serverIP,serverPort)
